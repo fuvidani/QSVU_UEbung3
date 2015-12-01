f938a0232f58b14ab2b75ffc7ee9c9d172c09464
Daniel Füvesi <e1326576@student.tuwien.ac.at>
2015-12-01 17:43:10 +0100

    Implement first test-case for calculateHypotenuse
    Add an implementation of a positive test-case, that should return the
    calculated hypotenuse. There is still no implementation, thus the test
    fails.

--- src/main/java/tdd/QSVU_UEbung3_1326576_Fuevesi_Daniel_TriangleTest.java
+++ src/main/java/tdd/QSVU_UEbung3_1326576_Fuevesi_Daniel_TriangleTest.java
@@ -1,5 +1,10 @@
 package main.java.tdd;
 
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 /**
  * <h4>About this class</h4>
  * <p>Description</p>
@@ -9,4 +14,26 @@ package main.java.tdd;
  * @since 01.12.15
  */
 public class QSVU_UEbung3_1326576_Fuevesi_Daniel_TriangleTest {
+
+    private QSVU_UEbung3_1326576_Fuevesi_Daniel_Triangle triangle;
+    private double cathetus1 = 22.5;
+    private double cathetus2 = 33;
+    private double angle1 = 34.3;
+
+    @Before
+    public void setUp(){
+        triangle = new QSVU_UEbung3_1326576_Fuevesi_Daniel_Triangle();
+    }
+
+    @After
+    public void tearDown(){
+        triangle = null;
+    }
+
+    @Test
+    public void calculateHypotenuse_ShouldReturnCorrectResult(){
+        double result = triangle.calculateHypotenuse(cathetus1,cathetus2);
+        Assert.assertEquals(39.9,result,0.0001);
+    }
+
 }
